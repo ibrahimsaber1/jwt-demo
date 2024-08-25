b05cdc2470efa8afd13487bf2f3f6f05f9ccf3ad
IBRAHIM SABER <ibrahimsamosee@gmail.com>
2024-08-25 23:57:29 +0300

    add userschema

--- index.js
+++ index.js
@@ -3,6 +3,28 @@ const mongoose = require("mongoose");
 
 // database connection :)
 
-mongoose.connect("mongodg//localhost:27017/AUTH").then(() => {
-  console.log("DB connection Successfully :)");
+mongoose
+  .connect("mongodg//localhost:27017/AUTH")
+  .then(() => {
+    console.log("DB connection Successfully :)");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+// schema
+
+const userschema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
 });

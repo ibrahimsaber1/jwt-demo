babaa5d0382ce7f7f65f49b26de4536d95f34d29
IBRAHIM SABER <ibrahimsamosee@gmail.com>
2024-08-26 20:07:00 +0300

    create post register end point

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 // database connection :)
 
 mongoose
-  .connect("mongodg//localhost:27017/AUTH")
+  .connect("mongodb://localhost:27017/AUTH")
   .then(() => {
     console.log("DB connection Successfully :)");
   })
@@ -27,4 +27,31 @@ const userschema = mongoose.Schema({
     type: String,
     required: true,
   },
+});
+
+// model for user
+
+const userModel = mongoose.model("users", userschema);
+
+// endpoints using EXPRESS :)
+
+const app = express();
+
+app.use(express.json()); // to reseve the data chunk by chunk .......
+
+app.post("/register", (req, res) => {
+  let user = req.body;
+  userModel
+    .create(user)
+    .then((doc) => {
+      res.send({ message: "User Registration Successfully" });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.send({ message: "Some Problem" });
+    });
+});
+
+app.listen(5222, () => {
+  console.log("successfully conecting to port 5222");
 });
